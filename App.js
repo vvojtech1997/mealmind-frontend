45660f697fbd4b5cc2d3a41882765f4bc7d47e1c
vvojtech1997 <v.vojtech1997@gmail.com>
2025-10-27 16:01:37 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,4 @@
+// App.js main file - open in Expo
+import React from 'react';
+import { Text, View } from 'react-native';
+export default function App(){ return (<View style={{flex:1,justifyContent:'center',alignItems:'center'}}><Text>MealMind - otvor v Expo</Text></View>);}
---
+++ config.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://mealmind-backend.onrender.com';
